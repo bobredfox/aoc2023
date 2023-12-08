eaaa93d8a8e7cc83f5f820a3dd66145d142c0d45
bobredfox <gelberwolf@posteo.de>
2023-12-08 19:26:42 +0100

    Nearly done day07 part 1

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,3 +1,27 @@
+use camelcards::camel_cards::{Cards, Hand, Types};
+use std::fs;
+
+mod camelcards;
+
+fn read_input(str_path: &str) -> Vec<Hand> {
+    let string = fs::read_to_string(str_path).unwrap();
+    let lines: Vec<&str> = string.lines().collect();
+    return lines.iter().map(|x| split_line(x)).collect();
+}
+
+fn split_line(line: &str) -> Hand {
+    return Hand::new(
+        line.split_whitespace().take(1).last().unwrap(),
+        line.split_whitespace()
+            .last()
+            .unwrap()
+            .to_owned()
+            .parse::<usize>()
+            .unwrap(),
+    );
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hands = read_input("input.data");
+    println!("{:?}", hands);
 }
